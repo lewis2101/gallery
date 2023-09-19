588a618defd27a08784a1f9d4d2698dd4247b808
lewis2101 <jacobfire21@gmail.com>
2023-09-19 15:56:20 +0600

    rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,10 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
-        pluginRewriteAll(),
-        createHtmlPlugin({
-            minify: true,
-            entry: 'src/main.ts',
-            template: 'build/vite/index.vite.html',
-            inject: {
-                tags: [
-                    {
-                        injectTo: 'head-prepend',
-                        tag: 'base',
-                        attrs: {
-                            href: '/'
-                        }
-                    }
-                ]
-            }
-        })
     ],
     server: {
         host: 'localhost',
