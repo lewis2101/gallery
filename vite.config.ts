08bdfc339d171fba8983c7f7a0b14cc1476c2909
lewis2101 <jacobfire21@gmail.com>
2023-09-19 15:48:33 +0600

    rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), pluginRewriteAll()],
   server: {
     host: 'localhost',
     port: 3000
